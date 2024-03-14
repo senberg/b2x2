1c2d2f5977b573cc64f58a452b771f56e2925ad6
Staffan Enberg <senberg@live.se>
2024-03-14 18:13:50 +0100

    Initial commit

---
+++ src/main/java/com/senberg/b2x2/B2x2Application.java
@@ -0,0 +1,13 @@
+package com.senberg.b2x2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2x2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2x2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/senberg/b2x2/B2x2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senberg.b2x2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2x2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
